ed8318d70fb32d1160e37a04ae1a1720d97507e4
gedizz <gedizzletsplay@gmail.com>
2023-12-10 15:25:42 +0000

    improve UI

--- password_manager.py
+++ password_manager.py
@@ -1,28 +1,23 @@
 import tkinter as tk
-from tkinter import simpledialog, messagebox
+from tkinter import simpledialog, messagebox, ttk
 from cryptography.fernet import Fernet
 import sqlite3
-import os
+import keyring
 
-# Path for the key file
-key_file = 'encryption.key'
-
-# Function to load or create an encryption key
+# Using keyring, we can access windows' default key service
 def load_or_create_key():
-    if os.path.exists(key_file):
-        with open(key_file, 'rb') as file:
-            return file.read()
-    else:
-        key = Fernet.generate_key()
-        with open(key_file, 'wb') as file:
-            file.write(key)
-        return key
+    key = keyring.get_password('password_manager', 'encryption_key')
+    if key is None:
+        key = Fernet.generate_key().decode()
+        keyring.set_password('password_manager', 'encryption_key', key)
+    return key.encode()
 
 # Load or create the key and instantiate a Fernet object
 key = load_or_create_key()
 cipher_suite = Fernet(key)
 
-# Initialize database connection
+# Initialize database connection - typically this would be something secured over the web
+# currently we can access it locally which is not good
 conn = sqlite3.connect('passwords.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS passwords
@@ -92,20 +87,36 @@ def remove_password():
     if selected_service:
         delete_password(selected_service)
 
-
 # Creating main window
 root = tk.Tk()
 root.title("Password Manager")
 
-# Layout
-tk.Button(root, text="Add Password", command=save_password).grid(row=0, column=0)
-tk.Button(root, text="Show Password", command=show_password).grid(row=0, column=1)
-tk.Button(root, text="Delete Password", command=remove_password).grid(row=0, column=2)
+# Styling
+style = ttk.Style()
+style.configure('TButton', font=('Arial', 10))
+style.configure('TLabel', font=('Arial', 10))
+style.configure('TListbox', font=('Arial', 10))
 
-service_list = tk.Listbox(root)
-service_list.grid(row=1, columnspan=3)
+# Main frame
+main_frame = ttk.Frame(root, padding="10")
+main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+# Layout inside main frame
+ttk.Button(main_frame, text="Add Password", command=save_password).grid(row=0, column=0, padx=5, pady=5)
+ttk.Button(main_frame, text="Show Password", command=show_password).grid(row=0, column=1, padx=5, pady=5)
+ttk.Button(main_frame, text="Delete Password", command=remove_password).grid(row=0, column=2, padx=5, pady=5)
+
+service_list = tk.Listbox(main_frame, height=10, width=50)
+service_list.grid(row=1, columnspan=3, pady=5)
 update_service_list()
 
+# Resizable configuration
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+main_frame.grid_columnconfigure(0, weight=1)
+main_frame.grid_rowconfigure(1, weight=1)
 
+# Start the GUI
 root.mainloop()
+# Ensure the connection is closed properly
 conn.close()
