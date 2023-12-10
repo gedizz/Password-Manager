1b1ebfe5bf5f00b2c354e0c2647e755e7932740b
gedizz <gedizzletsplay@gmail.com>
2023-12-10 16:06:36 +0000

    add password strength

--- password_manager.py
+++ password_manager.py
@@ -41,29 +41,32 @@ def decrypt_password(encrypted_password):
 
 
 # Initialize database connection
-# ...
-
-# Initialize database connection
 conn = sqlite3.connect('passwords.db')
 c = conn.cursor()
 
-# Create tables if they don't exist
+# Create 'users' table if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS users
              (username TEXT PRIMARY KEY, password_hash TEXT)''')
 
-# Check if 'username' column exists in 'passwords' table and add it if it doesn't
+# Create 'passwords' table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS passwords
+             (id INTEGER PRIMARY KEY, username TEXT, service TEXT, encrypted_password TEXT)''')
+
+# Add 'username' column to 'passwords' table if it doesn't exist
 c.execute('''SELECT count(*) FROM pragma_table_info('passwords') WHERE name='username' ''')
 if c.fetchone()[0] == 0:
     c.execute('''ALTER TABLE passwords ADD COLUMN username TEXT''')
 
-c.execute('''CREATE TABLE IF NOT EXISTS passwords
-             (id INTEGER PRIMARY KEY, username TEXT, service TEXT, encrypted_password TEXT)''')
+# Add 'strength' column to 'passwords' table if it doesn't exist
+c.execute('''SELECT count(*) FROM pragma_table_info('passwords') WHERE name='strength' ''')
+if c.fetchone()[0] == 0:
+    c.execute('''ALTER TABLE passwords ADD COLUMN strength TEXT''')
 
 conn.commit()
 
 
 
-
+# Utils
 def create_user(username, password):
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     c.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, hashed))
@@ -78,13 +81,31 @@ def verify_user(username, password):
     return False
 
 
+def assess_password_strength(password):
+    length = len(password)
+    has_numbers = any(char.isdigit() for char in password)
+    has_uppercase = any(char.isupper() for char in password)
+    has_lowercase = any(char.islower() for char in password)
+    has_special = any(not char.isalnum() for char in password)
+
+    if length >= 8 and has_numbers and has_uppercase and has_lowercase and has_special:
+        return "Strong"
+    elif length >= 8 and has_numbers and (has_uppercase or has_lowercase):
+        return "Medium"
+    else:
+        return "Weak"
+
+
+
 # Database interaction functions
 def add_password(username, service, password):
     encrypted_password = encrypt_password(password)
-    c.execute("INSERT INTO passwords (username, service, encrypted_password) VALUES (?, ?, ?)",
-              (username, service, encrypted_password))
+    password_strength = assess_password_strength(password)
+    c.execute("INSERT INTO passwords (username, service, encrypted_password, strength) VALUES (?, ?, ?, ?)",
+              (username, service, encrypted_password, password_strength))
     conn.commit()
     update_service_list(username)
+
 
 def get_password(username, service):
     c.execute("SELECT encrypted_password FROM passwords WHERE username=? AND service=?",
@@ -100,37 +121,76 @@ def delete_password(username, service):
     update_service_list(username)
 
 def update_service_list(username):
-    service_list.delete(0, tk.END)
-    c.execute("SELECT service FROM passwords WHERE username=?", (username,))
-    for service in c.fetchall():
-        service_list.insert(tk.END, service[0])
+    service_list.delete(*service_list.get_children())
+    c.execute("SELECT service, strength FROM passwords WHERE username=?", (username,))
+    for i, (service, strength) in enumerate(c.fetchall()):
+        color = '#ffcccb' if strength == 'Weak' else '#ffffcc' if strength == 'Medium' else '#ccffcc'
+        tag_name = f'colored_{i}'  # Unique tag for each row
+        service_list.insert('', 'end', values=(service, strength), tags=(tag_name,))
+        service_list.tag_configure(tag_name, background=color)
+
+
 
 
 # UI functions
 def save_password():
-    service = simpledialog.askstring("Service", "Enter the name of the service:")
+    service = custom_askstring("Service", "Enter the name of the service:")
     if service:
-        password = simpledialog.askstring("Password", "Enter the password:", show="*")
+        password = custom_askstring("Password", "Enter the password:", show="*")
         if password:
             add_password(current_user, service, password)
 
+
 def show_password():
-    selected_service = service_list.get(tk.ANCHOR)
-    if selected_service:
-        password = get_password(current_user, selected_service)
-        if password:
-            messagebox.showinfo("Password Info", f"Password for {selected_service}: {password}")
+    selected_item = service_list.selection()
+    if selected_item:
+        # Get the first selected item (assuming single selection)
+        item = service_list.item(selected_item[0])
+        selected_service = item['values'][0]  # Assuming service is the first value
+        c.execute("SELECT encrypted_password FROM passwords WHERE username=? AND service=?",
+                  (current_user, selected_service))
+        result = c.fetchone()
+        if result:
+            decrypted_password = decrypt_password(result[0])
+            messagebox.showinfo("Password Info", f"Password for {selected_service}: {decrypted_password}")
         else:
             messagebox.showinfo("Password Info", "Password not found")
 
+
+
 def remove_password():
-    selected_service = service_list.get(tk.ANCHOR)
-    if selected_service:
+    selected_items = service_list.selection()
+    if selected_items:
+        selected_service = service_list.item(selected_items[0])['values'][0]  # Assuming service is the first value
         delete_password(current_user, selected_service)
 
 
+def custom_askstring(title, prompt, show=None):
+    def on_ok():
+        nonlocal user_input
+        user_input = entry.get()
+        dialog.destroy()
 
-# Login Functionality
+    user_input = None
+    dialog = tk.Toplevel(root)
+    dialog.title(title)
+    dialog.grab_set()  # Makes the dialog modal
+
+    tk.Label(dialog, text=prompt).pack(padx=10, pady=10)
+    entry = ttk.Entry(dialog, show=show)
+    entry.pack(padx=10, pady=10)
+    entry.focus_set()
+
+    ok_button = ttk.Button(dialog, text="OK", command=on_ok)
+    ok_button.pack(pady=(0, 10))
+
+    dialog.transient(root)  # Dialog window is related to the main app window
+    dialog.wait_window()  # Wait here until the dialog is closed
+
+    return user_input
+
+
+# Login/logout Functionality
 def login():
     global current_user
     username = username_entry.get()
@@ -197,14 +257,25 @@ ttk.Button(main_frame, text="Add Password", command=save_password).grid(row=0, c
 ttk.Button(main_frame, text="Show Password", command=show_password).grid(row=0, column=1, padx=5, pady=5)
 ttk.Button(main_frame, text="Delete Password", command=remove_password).grid(row=0, column=2, padx=5, pady=5)
 
-service_list = tk.Listbox(main_frame, height=10, width=50)
-service_list.grid(row=1, columnspan=3, pady=5)
+# Define columns
+columns = ('service', 'strength')
+service_list = ttk.Treeview(main_frame, columns=columns, show='headings')
+service_list.heading('service', text='Service')
+service_list.heading('strength', text='Strength')
+service_list.column('service', width=150)
+service_list.column('strength', width=100)
+
+# Position the Treeview
+service_list.grid(row=1, columnspan=3, pady=5, sticky='nsew')
+
+# To allow the columns to be resized
+
 
 
 # Resizable configuration
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
-main_frame.grid_columnconfigure(0, weight=1)
+main_frame.grid_columnconfigure(1, weight=1)
 main_frame.grid_rowconfigure(1, weight=1)
 
 # Initially hide main frame
